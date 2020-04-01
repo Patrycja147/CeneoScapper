8b5d60d05762210a5ac438173d5034f0ac6ae948
Patrycja147 <patrycja.zolkiewska1@gmail.com>
2020-04-01 14:50:36 +0200

    PŻ 2020.04.01

--- scraper.py
+++ scraper.py
@@ -14,6 +14,19 @@ def extract_feature(opinion, tag, tag_class, child=None):
     except AttributeError:
         return None
 
+
+tags = {
+    "recommendation":["div","product-review-summary","em"],
+    "stars":["div", "review-score-count"],
+    "content":['p','product-review-body'],
+    "author":["div", "review-name-line"],
+    "pros":["div","pros-cell",'ul'],
+    "cons":["div","cons-cell",'ul'],
+    "usefull":['button','vote-yes', "span"],
+    "useless":['button','vote-no',"span"],
+    "purchased":["div","product-review-pz","em"],
+}
+
 #adres url strony z opiniami
 url_prefix = "https://www.ceneo.pl"
 product_id = input("Podaj kod produktu: ")
@@ -29,43 +42,29 @@ while url:
     page_respons = requests.get(url)
     page_tree = BeautifulSoup(page_respons.text, 'html.parser')
    
-    opinions = page_tree.find_all('li','js-product-review')
+    opinions = page_tree.find_all('li','js_product-review')
 
-    
+    for opinion in opinions:
+            features = {key:extract_feature(opinion,*args)
+                        for key, args in tags.items()}
 
-tags = {
-    "recommendation":["div","product-review-summary","em"],
-    "stars":["div", "review-score-count"],
-    "content":['p','product-review-body'],
-    "author":["div", "review-name-line"],
-    "pros":["div","pros-cell",'ul'],
-    "cons":["div","cons-cell",'ul'],
-    "usefull":['button','vote-yes', "span"],
-    "useless":['button','vote-no',"span"],
-    "purchased":["div","product-review-pz","em"],
-}
+            features["purchased"] = (features["purchased"] == "Opinia potwierdzona zakupem")
 
-for opinion in opinions:
-        features = {key:extract_feature(opinion,*args)
-                    for key, args in tags.items()}
+            features['opinion_id'] =  opinion["data-entry-id"]
+            dates = opinion.find("span","review-time").find_all("time")
+            features['review_date'] = dates.pop(0)["datetime"]
+            try:
+                features['purchase_date']= dates.pop(0)["datetime"]
+            except IndexError:
+                features['purchase_date']= None
+            
+            opinions_list.append(features)
 
-        features["purchased"] = (features["purchased"] == "Opinia potwierdzona zakupem")
-
-        features[opinion_id] =  opinion["data-entry-id"]
-        dates = opinion.find("span","review-time").find_all("time")
-        review_date = dates.pop(0)["datetime"]
-        try:
-            purchase_date= dates.pop(0)["datetime"]
-        except IndexError:
-            purchase_date= None
-        
-        opinions_list.append(features)
-
-try:
-    url = url_prefix + page_tree.find("a","pagination_next")["href"]
-except TypeError:
-    url = None
-print(url)
+    try:
+        url = url_prefix + page_tree.find("a","pagination_next")["href"]
+    except TypeError:
+        url = None
+    print(url)
 
 
 
