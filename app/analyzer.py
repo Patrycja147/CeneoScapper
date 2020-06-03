f8621df9e54921c055728d160f3b7965b5ea4c3b
Patrycja Żółkiewska <56259388+Patrycja147@users.noreply.github.com>
2020-06-03 15:47:55 +0200

    PŻ 2020.06.03

--- app/analyzer.py
+++ app/analyzer.py
@@ -5,16 +5,14 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 #wyświetlenie zawartości katalogu z opiniami
-print(*os.listdir("./app/opinions_json"))
+print(*os.listdir("./opinions_json"))
 
 #wczytanie identyfikatora produktu, którego opinie będą analizowane
 product_id = input("Podaj kod produktu: ")
 
 #wczytanie do ramki danych opinii z pliku
-opinions = pd.read_json("./app/opinions_json/"+product_id+'.json')  
+opinions = pd.read_json("./opinions_json/"+product_id+'.json')  
 opinions = opinions.set_index("opinion_id") 
-
-opinions["stars"] = opinions["stars"].map(lambda x: float(x.split("/")[0].replace(",", ".")))
 
 #częstość występowania poszczególnej liczby gwiazdek
 stars = opinions["stars"].value_counts().sort_index().reindex(list(np.arange(0, 5.1, 0.5)), fill_value=0)
@@ -24,7 +22,7 @@ plt.xticks(rotation=0)
 ax.set_title("Częstość występowania poszczególnych ocen")
 ax.set_xlabel("liczba gwiazdek")
 ax.set_ylabel("liczba opinii")
-plt.savefig("./app/opinions_json/figures_png/"+product_id+'_bar.png')
+plt.savefig("./figures_png/"+product_id+'_bar.png')
 plt.close()
 
 #udział poszczególnych rekomandacji w ogólnej liczbie opinii
@@ -32,7 +30,7 @@ recommendation = opinions["recommendation"].value_counts()
 fig, ax = plt.subplots()
 recommendation.plot.pie(label="", autopct="%.1f%%", colors=['mediumseagreen', 'coral'])
 ax.set_title("Udział poszczególnych rekomandacji w ogólnej liczbie opinii")
-plt.savefig("./app/opinions_json/figures_png/"+product_id+'_pie.png')
+plt.savefig("./figures_png/"+product_id+'_pie.png')
 plt.close()
 
 #podstawowe startstyki
